3449e61cc3fc71fbd0b55b844e2d20a1da12649d
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-22 15:03:34 +0100

    esercizio continuato qua per sbaglio

--- main.js
+++ main.js
@@ -1,48 +1,81 @@
 let btn = document.getElementById("btn");
 let griglia = document.getElementById("griglia")
-
-
+let bombeTot=[]
+let bombe=[]
+let array =[]
 
 
 btn.addEventListener("click", function () {
     let difficolta = document.getElementById("difficolta").value;
     console.log(difficolta)
+    griglia.innerHTML=""
 
-    if(difficolta==1){
-        for(i=1; i<=100; i++){
-            let box = document.createElement("div")
-            box.classList.add("box1")
-            box.innerHTML=`${i}`
-            griglia.appendChild(box);
-            box.addEventListener("click", function(){
-                console.log(this)
-                this.classList.add("bg_box")
-            })
-        }
-        
-    }else if(difficolta==2){
-        for(i=1; i<=81; i++){
-            let box = document.createElement('div')
-            box.classList.add("box2")
-            box.innerHTML=`${i}`
-            griglia.appendChild(box);
+    let numeroCelle;
+    array=[]
+    bombeTot=[]
+    bombe=[]
 
-            box.addEventListener("click", function(){
-                console.log(this)
-                this.classList.add("bg_box")
-            })
-        }
-    }else if(difficolta ==3){
-        for(i=1; i<=49; i++){
-            let box = document.createElement("div")
-            box.classList.add("box3")
-            box.innerHTML=`${i}`
-            griglia.appendChild(box)
-            box.addEventListener("click", function(){
-                console.log(this)
-                this.classList.add("bg_box")
-            })
-        }
+    if(difficolta=="1"){
+        numeroCelle=100
+    }else if(difficolta=="2"){
+        numeroCelle=81
+    }else{
+        numeroCelle=49
     }
-    
+
+    for(y=1; y<=numeroCelle; y++){
+        array.push(y)
+    }
+
+    function shuffle(array) {
+        return array.sort(() => Math.random() - 0.5);
+    }
+
+    array=shuffle(array)
+    console.log(shuffle(array))
+
+     // array bombe
+     for(x=1; x <= numeroCelle; x++){
+        bombeTot.push(x)
+    }
+    bombeTot=shuffle(bombeTot)
+    console.log(`le bombe sono ${bombeTot}`)
+    for(z=0; z<16;z++){
+        bombe.push(bombeTot[z])
+    }
+    console.log(`le 16 bombe sono ${bombe}`)
+    // fine array bombe
+
+    for(i=0; i<numeroCelle;i++){
+        let box = document.createElement("div")
+        griglia.appendChild(box);
+        if(numeroCelle == 100){
+            box.classList.add("box1") 
+        }else if(numeroCelle==81){
+            box.classList.add("box2")
+        }else{
+            box.classList.add("box3")
+        }
+        box.innerHTML=array[i]
+        griglia.appendChild(box);
+       
+
+
+
+        box.addEventListener("click", function(){
+            console.log(this.innerText)
+            if(bombe.includes(parseInt(this.innerText))){
+                this.classList.add("bg_bomba")
+                box.removeEventListener("click", function(){
+                    
+                })
+            } else{
+                this.classList.add("bg_box")
+            }
+        })
+    }    
 })
+
+
+
+
